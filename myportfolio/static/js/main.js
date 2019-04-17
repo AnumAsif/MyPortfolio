dfd40021aab0282191fdd7afd1d44bdc6a588bd1
Anum Asif <anum@cockar.com>
2019-04-17 17:13:40 +0300

    Add responsivity to the navbar

--- myportfolio/static/js/main.js
+++ myportfolio/static/js/main.js
@@ -1,5 +1,22 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("pb-navbar").style.top = "0";
+  } else {
+    document.getElementById("pb-navbar").style.top = "-50px";
+  }
+}
+
 $('.open').on('click', () => {
     var $id = $(this.event.currentTarget).attr('id');
     console.log($id);
     $('#project-'+$id).show();     
+})
+
+$('.nav-item').on('click', ()=>{
+  $('#myNavbar').hide();
+})
+$('#toggle-btn').on('click', ()=>{
+  $('#myNavbar').toggle();
 })
